48b1a44f5b810ccfe1aef29879b35a4ddee84210
Ryley anne Bridges <932400076@bun2-206-12.acg.ac.nz>
2019-08-02 09:30:22 +1200

    Updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
   // console.log('ready');
 
+
   $.ajax({
-    url: 'https://api.geonet.org.nz/intensity?type=reported',
+    url: 'https://api.geonet.org.nz/intensity?type=measured',
     type: 'GET',
     dataType: 'json',
     success: function(dataFromJSON){
-      console.log(dataFromJSON);
+      (console.log("working")
     },
     error: function(){
       console.log('something has gone wrong');
