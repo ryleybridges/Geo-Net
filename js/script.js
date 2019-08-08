6f937aa49ca86034bb420f8ff1950971be60c35f
Ryley anne Bridges <932400076@bun2-206-12.acg.ac.nz>
2019-08-09 11:27:02 +1200

    Finished loading coordinates and markers onto map

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
-$(document).ready(function(){
-  // console.log('ready');
+google.maps.event.addDomListener(window, 'load', initMap);
 
+function initMap(){
   $.ajax({
     url: 'https://api.geonet.org.nz/intensity?type=measured',
     type: 'GET',
     dataType: 'json',
     success: function(dataFromJSON){
+      var newZealand = {lat: -41.838875, lng: 174.8860};
+      var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: newZealand});
+      console.log(dataFromJSON.features);
       for (var i = 0; i < dataFromJSON.features.length; i++) {
-        console.log(dataFromJSON.features[i].geometry.coordinates);
-        $('#listData').html(`<li>${dataFromJSON.features[i].geometry.coordinates}</li>`);
+        const mapCoordinates = dataFromJSON.features[i].geometry.coordinates;
+          var marker = new google.maps.Marker({
+            position: {
+                      lat: mapCoordinates[1],
+                      lng: mapCoordinates[0]
+                  },
+                  map: map,
+              });
       }
     },
     error: function(){
       console.log('bad');
     }
-  })
+  });
 
-
-});
+}
