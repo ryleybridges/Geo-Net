219fb293d64b0e179e1b8249a2e67eedc44d091b
Ryley anne Bridges <932400076@bun2-206-12.acg.ac.nz>
2019-08-09 09:46:04 +1200

    Got all coordinates in console

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
   // console.log('ready');
 
-
   $.ajax({
     url: 'https://api.geonet.org.nz/intensity?type=measured',
     type: 'GET',
     dataType: 'json',
     success: function(dataFromJSON){
-      console.log('the coordinates are ' + dataFromJSON.features[0].geometry.coordinates[0] + ' and ');
-      console.log(dataFromJSON.features[0].geometry.coordinates[1]);
+      for (var i = 0; i < dataFromJSON.features.length; i++) {
+        console.log(dataFromJSON.features[i].geometry.coordinates);
+        $('#listData').html(`<li>${dataFromJSON.features[i].geometry.coordinates}</li>`);
+      }
     },
     error: function(){
-      console.log("bad");
+      console.log('bad');
     }
   })
 
